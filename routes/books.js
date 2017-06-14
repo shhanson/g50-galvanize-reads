d7f2553c363b0027e22bd8ab2bf53685da93af1c
Sarah Hanson <sarahh.hanson@gmail.com>
2017-06-14 10:56:01 -0500

    Completed put method for books router

--- routes/books.js
+++ routes/books.js
@@ -12,6 +12,10 @@ const bodyParser = require('body-parser');
 
 router.use(bodyParser.json());
 
+function isValidID(id) {
+    return (id >= 1 && !Number.isNaN(id));
+}
+
 function getBook(id) {
     //Query to fetch a book from "books" with the given id
     return knex('books')
@@ -23,6 +27,10 @@ function getAuthorsForBook(bookID) {
     return knex('authors')
         .join('books_authors', 'authors.id', 'books_authors.author_id')
         .where('books_authors.book_id', bookID);
+}
+
+function deleteBookFromJoinTable(bookID) {
+    return knex('books_authors').where('book_id', bookID).del();
 }
 
 function getBookWithAuthors(bookID) {
@@ -37,7 +45,13 @@ function getBookWithAuthors(bookID) {
             let [book, authors] = result;
             return [book, authors];
         });
-    //Do I need a catch here? or will it be handled below?
+}
+
+function knexError(err) {
+    console.error(err);
+    err.status = 500;
+    knex.destroy();
+    return err;
 }
 
 //GET all books
@@ -49,34 +63,25 @@ router.get('/books', (req, res, next) => {
             });
         }) //END THEN
         .catch((err) => {
-            console.error(err);
-            err.status = 500;
-            next(err);
-            knex.destroy();
+            next(knexError(err));
         }) //END CATCH
 }); //END GET
 
 //GET a book with the specified id
 router.get('/books/:id', (req, res, next) => {
-    //Grab id from URL and check if it's valid
     const bookID = Number.parseInt(req.params.id);
-    if (bookID < 1 || Number.isNaN(bookID)) {
+    if (!isValidID(bookID)) {
         next();
     } else {
         getBookWithAuthors(bookID).then((result) => {
-                //'result' is an array with the following format:
-                // [ [array containing the requested book (length=1)] , [array of authors]]
                 res.render('pages/book', {
                     book: result[0][0],
                     authors: result[1]
                 });
             })
             .catch((err) => {
-                console.error(err);
-                err.status = 500;
-                next(err);
-                knex.destroy();
-            })
+                next(knexError(err));
+            });
     } //END ELSE
 }); //END GET :id
 
@@ -90,68 +95,77 @@ router.post('/books', (req, res, next) => {
         cover_url: req.body.cover_url
     }).then((idArray) => {
         getBook(idArray[0]).then((result) => {
-            res.render('pages/book', {book: result[0], authors: undefined});
+            res.render('pages/book', {
+                book: result[0],
+                authors: undefined
+            });
         }).catch((err) => {
-            console.error(err);
-            err.status = 500;
-            next(err);
-            knex.destroy();
+            next(knexError(err));
         });
 
     }).catch((err) => {
-        console.error(err);
-        err.status = 500;
-        next(err);
-        knex.destroy();
+        next(knexError(err));
     });
 });
 
 //PUT (edit) a book with the specified id
-router.put('/books/:id', (req, res) => {
+router.put('/books/:id', (req, res, next) => {
 
     let bookID = Number.parseInt(req.params.id);
-    if (bookID < 1 || Number.isNaN(bookID)) {
+    if (!isValidID(bookID)) {
         next();
     } else {
+        //Query to fetch the book to be updated
         getBook(bookID).then((result) => {
-                let book = result[0];
-                let updatedBook = {
-                    title: req.body.title || book.title,
-                    genre: req.body.genre || book.genre,
-                    description: req.body.description || book.description,
-                    cover_url: req.body.cover_url || book.cover_url
-                };
+            let bookOrig = result[0];
+            //If user did not provide info for a particular field, retain the former field info
+            let bookUpdated = {
+                title: req.body.title || bookOrig.title,
+                genre: req.body.genre || bookOrig.genre,
+                description: req.body.description || bookOrig.description,
+                cover_url: req.body.cover_url || bookOrig.cover_url
+            };
+            //Query to replace the book with the given ID with the updated book
+            knex('books').returning('id').where('id', bookID).update(bookUpdated).then((idArray) => {
+                //Query to fetch the book with its authors and render
+                getBookWithAuthors(idArray[0]).then((bookInfo) => {
+                        res.render('pages/book', {
+                            book: bookInfo[0][0],
+                            authors: bookInfo[1]
+                        });
+                    })
+                    .catch((err) => {
+                        next(knexError(err));
+                    });
 
-            })
-            .then((result) => {
-
-            })
-            .catch((err) => {
-                console.error(err);
-                err.status = 500;
-                next(err);
-                knex.destroy();
+            }).catch((err) => {
+                next(knexError(err));
             });
-    }
-
-});
-
-//DELETE a book with the specified id
-router.delete('/books/:id', (req, res) => {
-    let bookID = Number.parseInt(req.params.id);
-    if (bookID < 1 || Number.isNaN(bookID)) {
-        next();
-    } else {
-        knex('books').where('id', bookID).del().then((result) => {
-            // res.render('pages/books', {data: result});
-            res.sendStatus(200);
 
         }).catch((err) => {
-            console.error(err);
-            err.status = 500;
-            next(err);
-            knex.destroy();
+            next(knexError(err));
         });
+
+    } //END ELSE
+
+}); //END PUT
+
+//DELETE a book with the specified id
+router.delete('/books/:id', (req, res, next) => {
+    let bookID = Number.parseInt(req.params.id);
+    if (!isValidID(bookID)) {
+        next();
+    } else {
+        //book must be deleted from join table 'books_authors' before it can be removed from 'books'
+        deleteBookFromJoinTable(bookID).then( () => {
+            knex('books').where('id', bookID).del().then(()=> {
+                res.sendStatus(200);
+            }).catch((err) => {
+                next(knexError(err));
+            })
+        }).catch((err) => {
+            next(knexError(err));
+        })
     }
 });
 
