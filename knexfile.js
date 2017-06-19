c9064e75405d0244b623e34db682f6ba9a24f350
Sarah Hanson <sarahh.hanson@gmail.com>
2017-06-19 10:08:52 -0500

    Changed to production env in routes files

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 module.exports ={
     development: {
         client: 'pg',
-        connection: 'postgres://localhost/greads_dev'
+        connection: process.env.DATABASE_URL || 'postgres://localhost/greads_dev'
     },
 
     test: {
